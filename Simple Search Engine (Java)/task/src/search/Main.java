27f15c7cad8a6499f8f4bb71b50d650a927be7a9
Yurii Flys <flysyurii@gmail.com>
2023-08-17 04:43:22 +0300

    Simple Search Engine Program

--- Simple Search Engine (Java)/task/src/search/Main.java
+++ Simple Search Engine (Java)/task/src/search/Main.java
@@ -18,6 +18,7 @@ public class Main {
             System.out.println("No data file specified!");
             return;
         }
+
         List<String> data;
         try {
             data = Files.readAllLines(Paths.get(fileName));
